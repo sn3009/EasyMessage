703bc39848a9f8d4a3f90c09244128431000657d
root <root@Shuttle-PC>
2016-04-24 22:52:54 +0800

    add

---
+++ kafka-producer-sdk/src/main/java/org/kafka/producer/sdk/App.java
@@ -0,0 +1,13 @@
+package org.kafka.producer.sdk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ message-processor/src/main/java/org/message/processor/App.java
@@ -0,0 +1,13 @@
+package org.message.processor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
