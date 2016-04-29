0d4816a9ca6836684a58ce5e50f67dd60f7617c4
shuttle <sunningkf@feinno.com>
2016-04-29 11:31:32 +0800

    update test method

--- kafka-consumer-sdk/src/test/java/org/emsg/kafka_sdk/test/Test.java
+++ kafka-consumer-sdk/src/test/java/org/emsg/kafka_sdk/test/Test.java
@@ -5,7 +5,7 @@ import org.emsg.kafka_sdk.start.impl.StartWithThreadPartition;
 
 public class Test {
 	public static void main(String [] args){
-		StartWithPartitionInterface start = new StartWithThreadPartition<>();
+		StartWithPartitionInterface start = new StartWithThreadPartition<String, String>();
 		start.start("sdknew2", 0);
 	}
 }
